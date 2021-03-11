6bcc39fa24f1afd65f37ceb01634f2d9337b2d21
unknown <quantran2381@gmail.com>
2021-03-11 20:06:23 +0700

    #main

--- Test2/frontend/src/redux/configStore.js
+++ Test2/frontend/src/redux/configStore.js
@@ -1,5 +1,5 @@
 import { combineReducers, createStore } from "redux";
-import {thunk} from"redux-thunk"
+
 import CustomersReducer from "./Reducers/CustomersReducer";
 const rootReducer = combineReducers({
   Customer: CustomersReducer,
